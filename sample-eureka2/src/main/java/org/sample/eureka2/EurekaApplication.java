ba34293a0b7b28f1ee30452567965b40420a398f
李振上 <lizhenshang@u51.com>
2016-07-29 14:42:56 +0800

    ·þÎñ·¢ÏÖÖ÷±¸

---
+++ sample-eureka2/src/main/java/org/sample/eureka2/EurekaApplication.java
@@ -0,0 +1,14 @@
+package org.sample.eureka2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
